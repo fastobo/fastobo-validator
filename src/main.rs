2611d9ca7649d90cfac06cfa0b54a775a6d662c4
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2019-05-10 23:39:26 -0700

    Improve CLI output using `textwrap` and add ISBN check

--- src/main.rs
+++ src/main.rs
@@ -2,123 +2,143 @@
 //!
 //! ```cargo
 //! [dependencies]
+//! clap = { version = "2.33.0", features = ["color"] }
 //! colored = "1.7.0"
-//! clap = "2.33.0"
 //! fastobo = "0.1.0"
+//! failure = "0.1.5"
 //! ```
 
 extern crate clap;
 extern crate colored;
 extern crate fastobo;
+#[macro_use]
+extern crate failure;
+extern crate isbn;
+extern crate textwrap;
+
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::str::FromStr;
 
 use colored::*;
-use fastobo::ast::OboDoc;
+use fastobo::ast::*;
+use fastobo::error::Error as OboError;
+use fastobo::visit::Visit;
+use isbn::Isbn;
+use isbn::IsbnError;
+
+#[derive(Debug, Fail)]
+enum ValidationError {
+    #[fail(display = "{}", 0)]
+    ParserFailed(OboError),
+    #[fail(display = "invalid ISBN: `{}` ({:?})", 0, 1)]
+    InvalidIsbn(PrefixedIdent, IsbnError),
+}
+
+#[derive(Default)]
+struct IsbnChecker<'a> {
+    valid: HashSet<&'a PrefixedIdent>,
+    invalid: HashMap<&'a PrefixedIdent, IsbnError>,
+}
+
+impl<'a> Visit<'a> for IsbnChecker<'a> {
+    fn visit_prefixed_ident(&mut self, id: &'a PrefixedIdent) {
+        if id.prefix().as_str() == "ISBN" {
+            if let Err(e) = Isbn::from_str(id.local().as_str()) {
+                self.invalid.insert(id, e);
+            } else {
+                self.valid.insert(id);
+            }
+        }
+    }
+}
 
 fn main() {
+    // Parse the CLI arguments.
     let matches = clap::App::new(clap::crate_name!())
         .author(clap::crate_authors!(","))
         .version(clap::crate_version!())
         .about(clap::crate_description!())
-
-        .arg(clap::Arg::with_name("INPUT").required(true).multiple(true))
-        //      .short("c")
-        //      .long("config")
-        //      .value_name("FILE")
-        //      .help("Sets a custom config file")
-        //      .takes_value(true))
-        // .arg(Arg::with_name("INPUT")
-        //      .help("Sets the input file to use")
-        //      .required(true)
-        //      .index(1))
-        // .arg(Arg::with_name("v")
-        //      .short("v")
-        //      .multiple(true)
-        //      .help("Sets the level of verbosity"))
-        // .subcommand(SubCommand::with_name("test")
-        //             .about("controls testing features")
-        //             .version("1.3")
-        //             .author("Someone E. <someone_else@other.com>")
-        //             .arg(Arg::with_name("debug")
-        //                 .short("d")
-        //                 .help("print debug information verbosely")))
+        .arg(
+            clap::Arg::with_name("INPUT")
+                .required(true)
+                .multiple(true)
+                .help("The path to an OBO file"),
+        )
+        .arg(
+            clap::Arg::with_name("ISBN")
+                .short("I")
+                .long("ISBN")
+                .help("Syntactically validate ISBN identifiers"),
+        )
         .get_matches();
 
-
-    
-    let mut failures = Vec::new();
-
+    // Record all failures
+    let mut failures: HashMap<_, Vec<ValidationError>> = HashMap::new();
 
     for input in matches.values_of("INPUT").unwrap() {
-        let relpath = std::path::PathBuf::from(input);
-        let abspath = match std::fs::canonicalize(&relpath) {
-            Ok(p) => p,
-            Err(e) => {
-                println!(
-                    "{:>12} resolving path {}",
-                    "Failed".red().bold(),
-                    relpath.display(),
-                );
-                failures.push((relpath, fastobo::error::Error::from(e)));
-                continue;
-            }
-        };
+        // Resolve the path.
+        let path = std::path::PathBuf::from(input);
+        failures.insert(path.clone(), Vec::new());
 
-        let _doc = match OboDoc::from_file(&abspath) {
+        // Parse the file
+        println!("{:>12} `{}`", "Parsing".green().bold(), path.display());
+        let start = std::time::Instant::now();
+        let doc = match OboDoc::from_file(&path) {
             Ok(d) => {
                 println!(
-                    "{:>12} parsing {} ({})",
+                    "{:>12} parsing `{}` in {:.2}s",
                     "Finished".green().bold(),
-                    relpath.display(),
-                    abspath.display()
+                    path.display(),
+                    start.elapsed().as_millis() as f64 / 1000.0,
                 );
                 d
             }
             Err(e) => {
-                println!(
-                    "{:>12} parsing {} ({})",
-                    "Failed".red().bold(),
-                    relpath.display(),
-                    abspath.display()
-                );
-                failures.push((relpath, e));
+                println!("{:>12} parsing `{}`", "Failed".red().bold(), path.display());
+                failures.entry(path).or_default().push(ValidationError::ParserFailed(e));
                 continue;
             }
         };
-    }
 
-    if !failures.is_empty() {
-        for (path, error) in failures {
-            println!("{}", error)
+        // Validate ISBN identifiers
+        if matches.is_present("ISBN") {
+            println!(
+                "{:>12} ISBN codes in `{}`...",
+                "Checking".green().bold(),
+                &path.display()
+            );
+
+            let mut checker = IsbnChecker::default();
+            checker.visit_doc(&doc);
+
+            println!(
+                "{:>12} {} ISBN codes",
+                "Found".green().bold(),
+                checker.valid.len() + checker.invalid.len()
+            );
+
+            for (id, err) in checker.invalid {
+                failures.entry(path.clone()).or_default().push(ValidationError::InvalidIsbn(id.clone(), err));
+            }
         }
-        std::process::exit(1);
     }
 
-    // // Gets a value for config if supplied by user, or defaults to "default.conf"
-    // let config = matches.value_of("config").unwrap_or("default.conf");
-    // println!("Value for config: {}", config);
-    //
-    // // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
-    // // required we could have used an 'if let' to conditionally get the value)
-    // println!("Using input file: {}", matches.value_of("INPUT").unwrap());
-    //
-    // // Vary the output based on how many times the user used the "verbose" flag
-    // // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
-    // match matches.occurrences_of("v") {
-    //     0 => println!("No verbose info"),
-    //     1 => println!("Some verbose info"),
-    //     2 => println!("Tons of verbose info"),
-    //     3 | _ => println!("Don't be crazy"),
-    // }
-    //
-    // // You can handle information about subcommands by requesting their matches by name
-    // // (as below), requesting just the name used, or both at the same time
-    // if let Some(matches) = matches.subcommand_matches("test") {
-    //     if matches.is_present("debug") {
-    //         println!("Printing debug info...");
-    //     } else {
-    //         println!("Printing normally...");
-    //     }
-    // }
-    //
-    // // more program logic goes here...
+    // Display all errors and exit
+    let mut retcode = 0;
+    if !failures.is_empty() {
+        for (path, errors) in failures {
+            if errors.is_empty() {
+                println!("{:>12} validation of `{}`", "Completed".green().bold(), path.display());
+            } else {
+                retcode = 1;
+                println!("{:>12} validation of `{}` ({} errors)", "Failed".red().bold(), path.display(), errors.len());
+                for error in errors {
+                    print!("{}", textwrap::indent(&format!("{}", error), "             "));
+                }
+            }
+        }
+    }
+
+    std::process::exit(retcode);
 }
