1f0611f980f241cff6740068cd02b5ce65a9ab6f
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2019-08-07 15:18:53 +0200

    Attempt fixing Bintray deployment scripts in Travis-CI

--- ci/travis/linux/script.sh
+++ ci/travis/linux/script.sh
@@ -8,4 +8,4 @@ cargo check
 
 # --- Build static x86_64 binaries -------------------------------------------
 
-docker run --rm -it -v $(pwd):/home/rust/src -v $HOME/.cargo:/root/.cargo $IMG_LINUX64 cargo build --release
+docker run --rm -it -v $(pwd):/home/rust/src $IMG_LINUX64 cargo build --release
--- ci/travis/osx/after_success.sh
+++ ci/travis/osx/after_success.sh
@@ -17,11 +17,12 @@ fi
 
 # --- Compress all binaries --------------------------------------------------
 
-upx -9 target/release/fastobo-validator
+upx -9 target/release/fastobo-validator || :
 
 # --- Package x86_64-apple-darwin --------------------------------------------
 
-mkdir -p $TRAVIS_BUILD_DIR/dist
+cd $TRAVIS_BUILD_DIR
+mkdir -p dist
 tar czf dist/fastobo_validator-x86_64-apple-darwin.tar.gz \
   README.md CHANGELOG.md COPYING \
   -C target/release/ fastobo-validator
