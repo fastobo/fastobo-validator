e8fc5fbf4fabddabf07c6ff2a87ee5fafd07237a
Martin Larralde <martin.larralde@embl.de>
2022-02-21 12:57:52 +0100

    Remove outdated Travis-CI configuration files

--- ci/travis/linux/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-. $(dirname $(dirname $0))/functions.sh
-
-# --- Pull musl Linux build container ----------------------------------------
-
-docker pull $IMG_LINUX64
--- ci/travis/osx/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-. $(dirname $(dirname $0))/functions.sh
-
-# --- Force linking gettext to make envsubst available -----------------------
-
-brew link --force gettext 
